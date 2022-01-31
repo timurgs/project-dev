108d82e9d692d6839da6a2d24f96f109d305e3b8
Timur Galiev <timurgravirovsky@gmail.com>
2022-01-31 17:14:21 +0300

    new commit

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM python:3.9
 COPY . /app
 ENV PYTHONPATH=/app
 RUN pip install -r /app/requirements.txt
-#RUN python /app/manage.py migrate
 CMD gunicorn app.stocks_products.wsgi -b 0.0.0.0:8000
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from logistic.settings_local import SECRET_KEY, DATABASES
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
